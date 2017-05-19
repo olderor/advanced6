c62e2d3263283211096d1e3afbf7f18ef72ca9b8
Yevchenko Bohdan <98body@gmail.com>
2017-05-19 12:43:44 +0300

    add code architecture

--- main.cpp
+++ main.cpp
@@ -3,55 +3,114 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <map>
-#include <algorithm>
 
-#pragma warning(disable:4996)
+struct decimal_number_compressor {
+ public:
+    decimal_number_compressor(std::string &number) {
+        data = number;
+        data_length = data.length();
+    }
 
-std::vector<int> prefix_func;
-std::string str;
-int str_len;
-
-int best_prefix_func_index = 0;
-
-void calc_prefix_function() {
-    prefix_func.resize(str_len);
-    prefix_func[0] = 0;
-
-    int matched_prefix, candidate;
-    for (int current = 1; current < str.size() - 2; ++current) {
-        matched_prefix = current - 1;
-        candidate = prefix_func[matched_prefix];
-        while (candidate != 0 && str[str_len - current - 1] != str[str_len - candidate - 1]) {
-            matched_prefix = prefix_func[matched_prefix] - 1;
-            candidate = prefix_func[matched_prefix];
+    std::string compress() {
+        const int fractional_part_start_index = find_fractional_part_start_index();
+        if (fractional_part_start_index == -1) {
+            return data;
         }
 
-        if (candidate == 0) {
-            prefix_func[current] = str[str_len - current - 1] == str[str_len - 1];
-        } else {
-            prefix_func[current] = candidate + 1;
-        }
-        if (prefix_func[best_prefix_func_index] < prefix_func[current]) {
-            best_prefix_func_index = current;
+        set_prefix_function_data(fractional_part_start_index);
+        const int max_index = find_index_with_maximum_prefix_function_value();
+
+        return compress(max_index);
+    }
+
+ private:
+    std::vector<int> prefix_function_data;
+    std::string data;
+    int data_length;
+
+    void set_prefix_function_data(const int start_index) {
+        prefix_function_data.clear();
+        prefix_function_data.resize(data_length);
+        prefix_function_data[0] = 0;
+
+        int matched_prefix, candidate;
+        for (int current = 1; current < data_length - start_index; ++current) {
+            matched_prefix = current - 1;
+            candidate = prefix_function_data[matched_prefix];
+            while (candidate != 0 && data[data_length - current - 1] != data[data_length - candidate - 1]) {
+                matched_prefix = prefix_function_data[matched_prefix] - 1;
+                candidate = prefix_function_data[matched_prefix];
+            }
+
+            if (candidate == 0) {
+                prefix_function_data[current] = data[data_length - current - 1] == data[data_length - 1];
+            } else {
+                prefix_function_data[current] = candidate + 1;
+            }
         }
     }
+
+    const int find_fractional_part_start_index() const {
+        for (int i = 0; i < data_length; ++i) {
+            if (data[i] == ',' || data[i] == '.') {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    const int find_index_with_maximum_prefix_function_value() const {
+        int max_prefix_function_index = 0;
+        for (int i = 1; i < prefix_function_data.size(); ++i) {
+            if (prefix_function_data[i] > prefix_function_data[max_prefix_function_index]) {
+                max_prefix_function_index = i;
+            }
+        }
+        return max_prefix_function_index;
+    }
+
+    std::string compress(const int best_prefix_func_index) const {
+        std::string compressed_data = "";
+        for (int j = 0; j < data_length - best_prefix_func_index - 1; ++j) {
+            compressed_data += data[j];
+        }
+        compressed_data += '(';
+        for (int j = data_length - best_prefix_func_index - 1;
+            j < data_length - prefix_function_data[best_prefix_func_index]; ++j) {
+
+            compressed_data += data[j];
+        }
+        compressed_data += ')';
+    }
+};
+
+void read_data(
+    std::istream &_Istr,
+    std::string &data) {
+
+    _Istr >> data;
+}
+
+void print_data(
+    std::ostream  &_Ostr,
+    std::string &data) {
+
+    _Ostr << data;
+}
+
+std::string solve(std::string &data) {
+    decimal_number_compressor compressor = decimal_number_compressor(data);
+    return compressor.compress();
 }
 
 int main() {
-    std::cin >> str;
-    str_len = str.length();
+    std::string data;
 
-    calc_prefix_function();
+    read_data(std::cin, data);
 
-    for (int j = 0; j < str_len - best_prefix_func_index - 1; ++j) {
-        std::cout << str[j];
-    }
-    std::cout << "(";
-    for (int j = str_len - best_prefix_func_index - 1; 
-        j < str_len - prefix_func[best_prefix_func_index]; ++j) {
-        std::cout << str[j];
-    }
-    std::cout << ")\n";
+    std::string result = solve(data);
+
+    print_data(std::cout, result);
+
     return 0;
 }
