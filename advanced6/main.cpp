ed5a0705c15bd7f318fc3930a811452368929ab6
Yevchenko Bohdan <98body@gmail.com>
2017-06-10 22:10:27 +0300

    fix string appending issue & add more comments

--- advanced6/main.cpp
+++ advanced6/main.cpp
@@ -3,40 +3,38 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
 
 // Compress decimal number to the periodic decimal number.
 struct decimal_number_compressor {
- public:
+public:
     // Initialize with the given decimal number.
     explicit decimal_number_compressor(std::string &number);
-
+    
     // Try to compress the data.
     // Return periodic decimal number.
     std::string compress();
-
- private:
+    
+private:
     // Initial data.
     std::string data;
-
+    
     // Initialize prefix function.
     std::vector<int> get_prefix_function_data();
-
+    
     // Compress the given data to the new data with periodical part.
     // prefix_function_data - precomputed prefix function data.
     // Return the periodic decimal number.
-    std::string compress(
-        std::vector<int> &prefix_function_data) const;
+    std::string compress(std::vector<int> &prefix_function_data) const;
 };
 
 // Get data from input stream.
-void read_data(
-    std::istream &_Istr,
-    std::string &data);
+void read_data(std::istream &_Istr,
+               std::string &data);
 
 // Put data to output stream.
-void print_data(
-    std::ostream  &_Ostr,
-    std::string &data);
+void print_data(std::ostream  &_Ostr,
+                std::string &data);
 
 // Get answer to the given problem using given data.
 std::string solve(std::string &data);
@@ -45,7 +43,8 @@ std::string solve(std::string &data);
 int main();
 
 
-decimal_number_compressor::decimal_number_compressor(std::string &number) {
+decimal_number_compressor
+::decimal_number_compressor(std::string &number) {
     data = number;
 }
 
@@ -56,7 +55,8 @@ std::string decimal_number_compressor::compress() {
 
 std::vector<int> decimal_number_compressor
 ::get_prefix_function_data() {
-
+    
+    // Finding fractional part delimiter.
     int start_index = 0;
     for (int i = 0; i < data.length(); ++i) {
         if (data[i] == ',' || data[i] == '.') {
@@ -64,24 +64,26 @@ std::vector<int> decimal_number_compressor
             break;
         }
     }
-
+    
+    // Initializing prefix function data with default values.
     std::vector<int> prefix_function_data(data.length());
     prefix_function_data[0] = 0;
-
+    
+    // Computing prefix function.
     int matched_prefix, candidate;
     for (int current = 1; current < data.length() - start_index; ++current) {
         matched_prefix = current - 1;
         candidate = prefix_function_data[matched_prefix];
         while (candidate != 0 &&
-            data[data.length() - current - 1] !=
-            data[data.length() - candidate - 1]) {
+               data[data.length() - current - 1] !=
+               data[data.length() - candidate - 1]) {
             matched_prefix = prefix_function_data[matched_prefix] - 1;
             candidate = prefix_function_data[matched_prefix];
         }
-
+        
         if (candidate == 0) {
             prefix_function_data[current] =
-                data[data.length() - current - 1] == data[data.length() - 1];
+            data[data.length() - current - 1] == data[data.length() - 1];
         } else {
             prefix_function_data[current] = candidate + 1;
         }
@@ -91,7 +93,8 @@ std::vector<int> decimal_number_compressor
 
 std::string decimal_number_compressor
 ::compress(std::vector<int> &prefix_function_data) const {
-
+    
+    // Finding index of largest value in the prefix function data.
     int prefix_function_index = 0;
     for (int i = 1; i < prefix_function_data.size(); ++i) {
         if (prefix_function_data[i] >
@@ -99,32 +102,33 @@ std::string decimal_number_compressor
             prefix_function_index = i;
         }
     }
-
-    std::string compressed_data = "";
+    
+    // Creates new string stream and fills it with integer part, delimiter
+    // and fractional part which is not included into periodical part.
+    std::stringstream compressed_data;
     for (int j = 0; j < data.length() - prefix_function_index - 1; ++j) {
-        compressed_data += data[j];
+        compressed_data << data[j];
     }
-    compressed_data += '(';
-    for (int j = data.length() - prefix_function_index - 1;
-        j < data.length() - prefix_function_data[prefix_function_index];
-        ++j) {
-        compressed_data += data[j];
+    // Appends periodical part to the stringstream.
+    compressed_data << '(';
+    for (int j = (int)data.length() - prefix_function_index - 1;
+         j < data.length() - prefix_function_data[prefix_function_index];
+         ++j) {
+        compressed_data << data[j];
     }
-    compressed_data += ')';
-    return compressed_data;
+    compressed_data << ')';
+    return compressed_data.str();
 }
 
-void read_data(
-    std::istream &_Istr,
-    std::string &data) {
-
+void read_data(std::istream &_Istr,
+               std::string &data) {
+    
     _Istr >> data;
 }
 
-void print_data(
-    std::ostream  &_Ostr,
-    std::string &data) {
-
+void print_data(std::ostream  &_Ostr,
+                std::string &data) {
+    
     _Ostr << data;
 }
 
@@ -135,12 +139,12 @@ std::string solve(std::string &data) {
 
 int main() {
     std::string data;
-
+    
     read_data(std::cin, data);
-
+    
     std::string result = solve(data);
-
+    
     print_data(std::cout, result);
-
+    
     return 0;
 }
